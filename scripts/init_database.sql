d02f3aa100db15cffe38898ed15882a06cb5f56f
Sagar Chaandwani <sagarchaandwani1991@gmail.com>
2026-04-01 13:32:07 +0100

    Create Datawarehouse database and schemas
    Added SQL commands to create a database and schemas.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,30 @@
 
+====================================================================================================================
+Create Database and schemas
+====================================================================================================================
+
+  
+
+
+
+
+
+-- Create the Database 'Datawarehouse'
+
+USE master;
+
+-- CREATE DATABASE Datawarehouse;
+
+USE Datawarehouse;
+GO
+
+-- CREATE Schemas
+  
+CREATE SCHEMA bronze;
+GO
+  
+CREATE SCHEMA silver;
+GO
+  
+CREATE SCHEMA gold;
+GO
