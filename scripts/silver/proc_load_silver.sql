9823f833ab30b947716641a51ea87843c38dfb84
Sagar Chaandwani <sagarchaandwani1991@gmail.com>
2026-05-05 10:26:51 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -49,8 +49,8 @@ cst_id,
 cst_key,
 TRIM(cst_firstname) AS cst_firstname,
 TRIM(cst_lastname)AS cst_lastname,
-CASE WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
-     WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+     WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
 ELSE 'n/a' 
 END AS cst_material_status,
 CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
